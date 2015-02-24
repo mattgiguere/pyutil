681b7cf17335d76acebbb37c19fe391d2a2c89e5
Matt Giguere <matthew.giguere@gmail.com>
2015-02-23 20:27:15 -0500

    initialized testing
    stepped back with my .travis.yml file to only test the version of python
    that I'm currently using. I also added the first test file,
    `test_getChironSpec.py`, which has a trivial example for starters.

---
+++ tests/test_getChironSpec.py
@@ -0,0 +1,10 @@
+
+
+from __future__ import division, print_function
+
+def testingTests():
+	assert 1 == 1
+
+#def test_getChironSpec_1401171125():
+#	from getChironSpec import getChironSpec
+#	assert 
