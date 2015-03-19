e53b522244b7eab0777f45b90fc03cb319b9627b
Matt Giguere <matthew.giguere@gmail.com>
2015-03-19 16:23:31 -0400

    added binRvs
    a routine I am making to compute the weighted mean of RV measurements.
    This is useful if multiple observations are taken each night and
    you are interested in only one nightly observation, or one observation
    every few days.

---
+++ binRvs.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""
+Created on 2015-03-19T15:55:54
+"""
+
+from __future__ import division, print_function
+import sys
+import argparse
+
+try:
+    import numpy as np
+except ImportError:
+    print('You need numpy installed')
+    sys.exit(1)
+
+__author__ = "Matt Giguere (github: @mattgiguere)"
+__maintainer__ = "Matt Giguere"
+__email__ = "matthew.giguere@yale.edu"
+__status__ = " Development NOT(Prototype or Production)"
+__version__ = '0.0.1'
+
+
+def binRvs(arg1, arg2):
+    """
+    PURPOSE: A routine to bin RV measurements similar to velplot 
+    in IDL.
+    """
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='argparse object.')
+    parser.add_argument(
+        'arg1',
+        help='This argument does something.')
+    parser.add_argument(
+        'arg2',
+        help='This argument does something else. By specifying ' +
+             'the "nargs=>" makes this argument not required.',
+             nargs='?')
+    if len(sys.argv) > 3:
+        print('use the command')
+        print('python filename.py tablenum columnnum')
+        sys.exit(2)
+
+    args = parser.parse_args()
+
+    binRvs(int(args.arg1), args.arg2)
+ 
---
+++ tests/test_BinRvs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Created on 2015-03-19T16:19:41
+"""
+
+from __future__ import division, print_function
+import sys
+import argparse
+
+try:
+    import numpy as np
+except ImportError:
+    print('You need numpy installed')
+    sys.exit(1)
+
+__author__ = "Matt Giguere (github: @mattgiguere)"
+__maintainer__ = "Matt Giguere"
+__email__ = "matthew.giguere@yale.edu"
+__status__ = " Development NOT(Prototype or Production)"
+__version__ = '0.0.1'
+
+
+def test_binRvs(arg1, arg2):
+    """PURPOSE: To """
+
