eafb36c1b2f2a3ef20a35d429057a56a21444f9a
Matt Giguere <matthew.giguere@gmail.com>
2015-03-21 17:18:40 -0400

    added setup.py
    Now that the pyutil code is in a subdirectory (also named pyutil),
    I added the module path in the test script. However, travis-ci
    does not know where to find the module. To fix this
    
    - I created a setup.py file
    - added a `python setup.py install` line to the .travis.yml file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyutil',
+      version='1.0',
+      description="Matt's python utility files",
+      author='Matt Giguere',
+      author_email='matthew.giguere@gmail.com',
+      url='https://github.com/mattgiguere/pyutil/',
+      packages=['pyutil'],
+      )
